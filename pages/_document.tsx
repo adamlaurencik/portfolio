78c82206bcea2128e712087ab40d8fdbe14dccf9
Adam Laurencik <laurencik@inqool.cz>
2020-12-04 01:46:06 +0100

    header bugfix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,35 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        <title>Adam's Laurencik Portfolio</title>
+        <meta
+          name="description"
+          content="Portfolio webpage of Adam Laurencik, skilled FrontEnd developer. Learn about his work!"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="favicon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
         <body>
           <Main />
           <NextScript />
